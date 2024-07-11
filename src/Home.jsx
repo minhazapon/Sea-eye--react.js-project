15eafcc1edfa01e78ccfd2f5b97fb2206dae1622
minhazapon <minhajulabedinapon@gmail.com>
2024-07-11 09:15:29 -0700

    banner file

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,12 @@
+import Banner from "./home compo/Banner";
 
 
 
 const Home = () => {
     return (
         <div>
+
+            <Banner></Banner>
             
         </div>
     );
---
+++ src/home compo/Banner.jsx
@@ -0,0 +1,59 @@
+
+
+
+const Banner = () => {
+    return (
+        <div className=" mt-10 mb-10 ml-10 mr-10">
+
+            <div className=" flex-col md:flex-row">
+
+
+
+
+            <div className=" bg-no-repeat bg-cover  p-52 rounded-xl " style={{
+              backgroundImage: "url(  https://img.freepik.com/free-photo/old-sailing-ship-sails-through-dark-night-reflecting-water-generated-by-artificial-intelligence_188544-126489.jpg?t=st=1720713742~exp=1720717342~hmac=51aab114568745267a751af15732e93f2eeba80dbcb44b9709c79fb91e67e0a9&w=826  )",
+            }}>
+
+            
+            <div className=" flex-col md:flex-row text-white bg-gradient-to-r from-black p-5 ">
+
+            
+            <div className=" flex-col md:flex-row flex items-center gap-5">
+
+            <img className=" h-[100px]" src="https://i.ibb.co/275vd3y/sailing-boat.png" alt="" />    
+            <p className=" text-6xl font-serif">At Home <br></br> On All Roads</p>
+
+
+            </div>
+
+            <p className=" text-2xl font-serif">transport Company, since 2001</p>
+
+            </div>
+            
+
+
+
+
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Banner;
